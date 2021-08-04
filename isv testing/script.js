db9fee27de5d64c0e8ed8f72f68a3a47834d63a6
sarikahire20 <86787406+sarikahire20@users.noreply.github.com>
2021-08-04 18:20:36 +0530

    sarikahire20-patch-1

---
+++ isv testing/script.js
@@ -0,0 +1,25 @@
+(function($) {
+
+    "use strict"
+
+    $(function() {
+        function scrollBanner() {
+            $(document).scroll(function() {
+                var scrollPos = $(this).scrollTop();
+                $('#banner-text').css({
+                    'top': (scrollPos / 3) + 'px',
+                    'opacity': 1 - (scrollPos / 510)
+                });
+                $('#banner').css({
+                    'background-position': 'center ' + (-scrollPos / 3) + 'px'
+                });
+            });
+        }
+        scrollBanner();
+
+    });
+
+})(jQuery);
+
+
+
